2d2f6565bd750a92b570dfa1e399dd4501992a19
Mathilde Buenerd <mathilde.buenerd@shopify.com>
2023-07-18 16:18:41 +0200

    Install prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <h1>Home</h1>
-  )
+  return <h1>Home</h1>;
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,3 @@
 export default function Search() {
-    return <h1>Search</h1>;
+  return <h1>Search</h1>;
 }
