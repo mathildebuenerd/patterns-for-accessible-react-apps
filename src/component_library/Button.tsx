548c2e31ff140ee55bf0ea3f8902c8e240d27044
Mathilde Buenerd <mathilde.buenerd@shopify.com>
2023-09-05 13:46:48 +0200

    Add props to Button

--- src/component_library/Button.tsx
+++ src/component_library/Button.tsx
@@ -1,10 +1,16 @@
 "use client";
+import { PropsWithChildren } from "react";
 import styles from "./Button.module.css";
 
-interface Props {
-  label: string;
+interface Props extends PropsWithChildren {
+  onClick?: () => void;
+  type?: HTMLButtonElement["type"];
 }
 
-export default function Button({ label }: Props) {
-  return <button>{label}</button>;
+export default function Button({ children, type = "button", onClick }: Props) {
+  return (
+    <button type={type} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
