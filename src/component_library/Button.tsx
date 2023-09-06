6a3a6dac6b22f8a5ba50284fba8627a136666c3f
Mathilde Buenerd <mathilde.buenerd@shopify.com>
2023-09-06 09:44:23 +0200

    Button: improve styles

--- src/component_library/Button.tsx
+++ src/component_library/Button.tsx
@@ -5,11 +5,17 @@ import styles from "./Button.module.css";
 interface Props extends PropsWithChildren {
   onClick?: () => void;
   type?: HTMLButtonElement["type"];
+  variant?: "primary";
 }
 
-export default function Button({ children, type = "button", onClick }: Props) {
+export default function Button({
+  children,
+  type = "button",
+  onClick,
+  variant = "primary",
+}: Props) {
   return (
-    <button type={type} onClick={onClick}>
+    <button className={styles.primary} type={type} onClick={onClick}>
       {children}
     </button>
   );
