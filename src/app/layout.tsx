bdbe4e3fbb7f16334a5f7c8bdb5b6b3e5bfaab23
Mathilde Buenerd <mathilde.buenerd@shopify.com>
2023-09-13 12:04:13 +0200

    Change metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Story Seekers - Book store",
-  description:
-    "Embark on boundless adventures in captivating books at Story Seekers. Discover thrilling novels, fantasy epics, and heartwarming tales that will ignite your imagination.",
+  title: "Pokédex",
+  description: "",
 };
 
 export default function RootLayout({
